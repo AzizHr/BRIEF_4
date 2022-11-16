7aef69eb27a574715c67882861e26778539a7934
kandirou0 <105118673+kandirou0@users.noreply.github.com>
2022-11-16 11:09:26 +0100

    Update gaming_store.sql

--- LIVRABLE_2/gaming_store.sql
+++ LIVRABLE_2/gaming_store.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 15, 2022 at 05:07 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 16 nov. 2022 à 11:08
+-- Version du serveur : 10.4.25-MariaDB
+-- Version de PHP : 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gaming_store`
+-- Base de données : `gaming_store`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Structure de la table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -34,16 +34,16 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `admin`
+-- Déchargement des données de la table `admin`
 --
 
 INSERT INTO `admin` (`ID`, `USERNAME`, `PASSWORD`) VALUES
-(1, 'Youssef.Wakhidi.Admin', 'ADMIN123');
+(1, 'simo', 'admin');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Structure de la table `category`
 --
 
 CREATE TABLE `category` (
@@ -52,7 +52,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `category`
+-- Déchargement des données de la table `category`
 --
 
 INSERT INTO `category` (`ID_CAT`, `LIBELLE_CAT`) VALUES
@@ -63,7 +63,7 @@ INSERT INTO `category` (`ID_CAT`, `LIBELLE_CAT`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Structure de la table `product`
 --
 
 CREATE TABLE `product` (
@@ -76,74 +76,74 @@ CREATE TABLE `product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `product`
+-- Déchargement des données de la table `product`
 --
 
 INSERT INTO `product` (`ID`, `LIBELLE`, `QUANTITE`, `PRIX`, `IMAGE`, `ID_CAT`) VALUES
-(1, 'Computer1', 25, 500, '', 1),
-(2, 'Computer2', 12, 600, '', 2),
-(3, 'Computer3', 15, 400, '', 3),
-(5, 'Keyboard1', 35, 40, '', 1),
-(6, 'Keyboard2', 45, 45, '', 2),
-(7, 'Keyboard3', 25, 32, '', 3),
-(9, 'Mouse1', 47, 20, '', 1),
-(10, 'Mouse2', 57, 17, '', 2),
-(11, 'Mouse3', 60, 24, '', 3),
-(13, 'Speaker1', 31, 70, '', 1),
-(14, 'Speaker2', 37, 52, '', 2),
-(15, 'Speaker3', 87, 95, '', 3);
+(1, 'TEST', 25, 500, 'computer1.jpg', 1),
+(2, 'Computer2', 12, 600, 'computer2.webp', 2),
+(3, 'Computer3', 15, 400, 'computer3.jpg', 3),
+(5, 'Keyboard1', 35, 40, 'keyboard1.jpg', 1),
+(6, 'Keyboard2', 45, 45, 'keyboard2.jpg', 2),
+(7, 'Keyboard3', 25, 32, 'keyboard3.jpg', 3),
+(9, 'Mouse1', 47, 20, 'mouse1.jpg', 1),
+(10, 'Mouse2', 57, 17, 'mouse2.jpeg', 2),
+(11, 'Mouse3', 60, 24, 'mouse3.jpg', 3),
+(13, 'Speaker1', 31, 70, 'speaker1.jpg', 1),
+(14, 'Speaker2', 37, 52, 'speaker2.jpg', 2),
+(15, 'Speaker3', 87, 95, 'speaker3.jpg', 3);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `admin`
+-- Index pour la table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `category`
+-- Index pour la table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`ID_CAT`);
 
 --
--- Indexes for table `product`
+-- Index pour la table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `product_ibfk_1` (`ID_CAT`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
   MODIFY `ID_CAT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `product`
+-- Contraintes pour la table `product`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`ID_CAT`) REFERENCES `category` (`ID_CAT`) ON DELETE CASCADE ON UPDATE CASCADE;

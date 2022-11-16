ce111e3eb9a551afda4640332b3c60bfd979d395
kandirou0 <simo.kandirou15@gmail.com>
2022-11-16 17:43:30 +0100

    update kn

--- LIVRABLE_3&4/Gaming_Store/admin/update.php
+++ LIVRABLE_3&4/Gaming_Store/admin/update.php
@@ -1,24 +1,19 @@
 <?php
     require_once '../php/database.php';
     
-    $id=$_POST["id"];
-    // $libelle = $_POST["libelle"];
-    // $quantite = $_POST["quantite"];
-    // $prix = $_POST["prix"];
-    // $image = $_FILES['image']['name'];
+    $id=$_GET["id"];
+    $libelle = $_GET["libelle"];
+    $quantite = $_GET["quantite"];
+    $prix = $_GET["prix"];
+    $image = $_FILES['image']['name'];
 
-    // $query = $conn->prepare("UPDATE product SET LIBELLE = :L , QUANTITE = :Q , PRIX = :P, IMAGE = :I WHERE ID=:id");
-    // $query->bindParam(":L" , $libelle);
-    // $query->bindParam(":Q" , $quantite);
-    // $query->bindParam(":P" , $prix);
-    // $query->bindParam(":I" , $image);
-    // $query->bindParam(":id" , $id);
-    // $query->execute();
-    // header("location:management.php");
-    // exit();
-    echo $id.'<br>';
-    // echo $libelle.'<br>';
-    // echo $quantite.'<br>';
-    // echo $prix.'<br>';
-    // echo $image.'<br>';
+    $query = $conn->prepare("UPDATE product SET LIBELLE = :L , QUANTITE = :Q , PRIX = :P, IMAGE = :I WHERE ID=:id");
+    $query->bindParam(":L" , $libelle);
+    $query->bindParam(":Q" , $quantite);
+    $query->bindParam(":P" , $prix);
+    $query->bindParam(":I" , $image);
+    $query->bindParam(":id" , $id);
+    $query->execute();
+    header("location:management.php");
+    exit();
 ?>

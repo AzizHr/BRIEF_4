890a1439b85a5be437594e5b148a6e82f16c4763
kandirou0 <simo.kandirou15@gmail.com>
2022-11-16 09:45:36 +0100

    update kn

--- LIVRABLE_3&4/Gaming_Store/php/database.php
+++ LIVRABLE_3&4/Gaming_Store/php/database.php
@@ -9,6 +9,7 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
+  session_start();
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }

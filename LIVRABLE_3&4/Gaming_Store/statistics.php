739dd9b551c923455bf0c0e4c3a04ff7b44f28cd
AzizHr <azizharkati20@gmail.com>
2022-11-17 11:27:25 +0100

    update Hr

---
+++ LIVRABLE_3&4/Gaming_Store/statistics.php
@@ -0,0 +1,47 @@
+<?php 
+require_once 'header.php' 
+?>
+<link rel="stylesheet" href="assets/css/statistics.css">
+    <div class="container align-content-center" style="margin-top:100px;">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="sts_h1">
+                    <div class="img_h1">
+                        <img src="images/total_products.png" alt="" class="i_m_g">
+                    </div>
+                    <h3>100</h3>
+                    <p>Total Products</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="sts_h2">
+                    <div class="img_h2">
+                        <img src="images/max.png" alt="" class="i_m_g">
+                    </div>
+                    <h3>100</h3>
+                    <p>Max Price</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="sts_h3">
+                    <div class="img_h3">
+                        <img src="images/min.png" alt="" class="i_m_g">
+                    </div>
+                    <h3>100</h3>
+                    <p>Min Price</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="sts_h4">
+                    <div class="img_h4">
+                        <img src="images/average.png" alt="" class="i_m_g">
+                    </div>
+                    <h3>100</h3>
+                    <p>Prices Average</p>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php 
+  require_once 'footer.php' 
+?>

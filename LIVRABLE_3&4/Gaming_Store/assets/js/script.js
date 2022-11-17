34b103027f148b3a0c9f20d77ff77e57641f4569
kandirou0 <simo.kandirou15@gmail.com>
2022-11-17 14:41:38 +0100

    update kn

--- LIVRABLE_3&4/Gaming_Store/assets/js/script.js
+++ LIVRABLE_3&4/Gaming_Store/assets/js/script.js
@@ -1,37 +1,3 @@
-// var computer = document.getElementById("computers");
-// var keyboard = document.getElementById("keyboards");
-// var mouse = document.getElementById("mouses");
-// var speaker = document.getElementById("speakers");
-
-
-// computer.addEventListener("click" , function(){
-//     computer.classList.add("selected");
-//     keyboard.classList.remove("selected");
-//     mouse.classList.remove("selected");
-//     speaker.classList.remove("selected");
-// });
-
-// keyboard.addEventListener("click" , function(){
-//     keyboard.classList.add("selected");
-//     computer.classList.remove("selected");
-//     mouse.classList.remove("selected");
-//     speaker.classList.remove("selected");
-// });
-
-// mouse.addEventListener("click" , function(){
-//     mouse.classList.add("selected");
-//     computer.classList.remove("selected");
-//     mouse.classList.remove("selected");
-//     speaker.classList.remove("selected");
-// });
-
-// speaker.addEventListener("click" , function(){
-//     speaker.classList.add("selected");
-//     computer.classList.remove("selected");
-//     mouse.classList.remove("selected");
-//     speaker.classList.remove("selected");
-// });
-
 
 var categories=document.querySelectorAll('.category');
 categories.forEach(cat=>{

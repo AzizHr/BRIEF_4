c094f924b9e85032f6d600a84459e89cab27e8b5
AzizHr <azizharkati20@gmail.com>
2022-11-24 14:26:22 +0100

    update HR

--- LIVRABLE_3&4/Gaming_Store/admin/management.php
+++ LIVRABLE_3&4/Gaming_Store/admin/management.php
@@ -21,8 +21,8 @@ else
     <title>Management</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
-
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container">
@@ -51,7 +51,7 @@ else
         </div>
     </nav>
     <div class="container mt-5 justify-content-center">
-        <table class="table">
+        <table class="table" style="width:100%;">
             <thead>
                 <th>
                     <p class="text-center">Image</p>
